4ddc5281fde0bfb4c6fb90611b3812e5ea2ca5a4
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2017-07-04 13:11:49 +0200

    fix scripts

--- compile-thesis-windows-luatex.bat
+++ compile-thesis-windows-luatex.bat
@@ -1,32 +1,37 @@
-SET FILENAME = %1
-SET OUTPUT = %2
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+SET arg[0]=%1
+SET arg[1]=%2
+
+if "!arg[0]!"=="" ( set FILENAME=thesis)else ( set FILENAME=!arg[0]!)
+if "!arg[1]!"=="" ( set OUTPUT=thesis_luatex)else ( set OUTPUT=!arg[1]!)
 rem MIKTEX_BIN=C:\miktex\texmfs\install\miktex\bin\
 rem set PATH=%MIKTEX_BIN%;%PATH%
 
-DEL /Y  "%FILENAME%.aux"
-DEL  /Y "%FILENAME%.bbl"
-DEL /Y "%FILENAME%.blg"
-DEL /Y "%FILENAME%.d"
-DEL /Y "%FILENAME%.fls"
-DEL /Y "%FILENAME%.ild"
-DEL /Y "%FILENAME%.ind"
-DEL /Y "%FILENAME%.toc"
-DEL /Y "%FILENAME%.lot"
-DEL /Y "%FILENAME%.lof"
-DEL /Y "%FILENAME%.idx"
-DEL /Y "%FILENAME%.out"
-DEL /Y "%FILENAME%.nlo"
-DEL /Y "%FILENAME%.nls"
-DEL /Y "%FILENAME%.pdf"
-DEL /Y "%FILENAME%.ps"
-DEL /Y "%FILENAME%.dvi"
-DEL /Y "%FILENAME%.bcf"
-DEL /Y "%FILENAME%.aux.bbl"
-DEL /Y "%FILENAME%.aux.blg"
+DEL   "%FILENAME%.aux"
+DEL   "%FILENAME%.bbl"
+DEL  "%FILENAME%.blg"
+DEL  "%FILENAME%.d"
+DEL  "%FILENAME%.fls"
+DEL  "%FILENAME%.ild"
+DEL  "%FILENAME%.ind"
+DEL  "%FILENAME%.toc"
+DEL  "%FILENAME%.lot"
+DEL  "%FILENAME%.lof"
+DEL  "%FILENAME%.idx"
+DEL  "%FILENAME%.out"
+DEL  "%FILENAME%.nlo"
+DEL  "%FILENAME%.nls"
+DEL  "%FILENAME%.pdf"
+DEL  "%FILENAME%.ps"
+DEL  "%FILENAME%.dvi"
+DEL  "%FILENAME%.bcf"
+DEL  "%FILENAME%.aux.bbl"
+DEL  "%FILENAME%.aux.blg"
 
 
 
-DEL /Y "%FILENAME%.aux"
+DEL  "%FILENAME%.aux"
 DEL "%FILENAME%.acn"
 DEL "%FILENAME%.acr"
 DEL "%FILENAME%.alg"
@@ -62,7 +67,7 @@ makeglossaries "%FILENAME%"
 lualatex --shell-escape --draftmode --synctex=1  --interaction=nonstopmode  "%FILENAME%.tex"
 makeglossaries "%FILENAME%"
 lualatex --shell-escape  --synctex=1  --interaction=nonstopmode "%FILENAME%.tex"
-MOVE /Y "%FILENAME%.pdf" "%OUTPUT%.pdf"
+MOVE /Y %FILENAME%.pdf %OUTPUT%.pdf
 countErrWrnBBx.bat "%FILENAME%.log"
 
 
--- compile-thesis-windows.bat
+++ compile-thesis-windows.bat
@@ -1,25 +1,31 @@
-SET FILENAME=%1
+SETLOCAL EnableExtensions EnableDelayedExpansion
 
-DEL /Y  "%FILENAME%.aux"
-DEL  /Y "%FILENAME%.bbl"
-DEL /Y "%FILENAME%.blg"
-DEL /Y "%FILENAME%.d"
-DEL /Y "%FILENAME%.fls"
-DEL /Y "%FILENAME%.ild"
-DEL /Y "%FILENAME%.ind"
-DEL /Y "%FILENAME%.toc"
-DEL /Y "%FILENAME%.lot"
-DEL /Y "%FILENAME%.lof"
-DEL /Y "%FILENAME%.idx"
-DEL /Y "%FILENAME%.out"
-DEL /Y "%FILENAME%.nlo"
-DEL /Y "%FILENAME%.nls"
-DEL /Y "%FILENAME%.pdf"
-DEL /Y "%FILENAME%.ps"
-DEL /Y "%FILENAME%.dvi"
-DEL /Y "%FILENAME%.bcf"
-DEL /Y "%FILENAME%.aux.bbl"
-DEL /Y "%FILENAME%.aux.blg"
+SET arg[0]=%1
+SET arg[1]=%2
+
+if "!arg[0]!"=="" ( set FILENAME=thesis)else ( set FILENAME=!arg[0]!)
+if "!arg[1]!"=="" ( set OUTPUT=thesis_luatex)else ( set OUTPUT=!arg[1]!)
+
+DEL   "%FILENAME%.aux"
+DEL   "%FILENAME%.bbl"
+DEL  "%FILENAME%.blg"
+DEL  "%FILENAME%.d"
+DEL  "%FILENAME%.fls"
+DEL  "%FILENAME%.ild"
+DEL  "%FILENAME%.ind"
+DEL  "%FILENAME%.toc"
+DEL  "%FILENAME%.lot"
+DEL  "%FILENAME%.lof"
+DEL  "%FILENAME%.idx"
+DEL  "%FILENAME%.out"
+DEL  "%FILENAME%.nlo"
+DEL  "%FILENAME%.nls"
+DEL  "%FILENAME%.pdf"
+DEL  "%FILENAME%.ps"
+DEL  "%FILENAME%.dvi"
+DEL  "%FILENAME%.bcf"
+DEL  "%FILENAME%.aux.bbl"
+DEL  "%FILENAME%.aux.blg"
 
 
 
@@ -59,6 +65,6 @@ makeglossaries "%FILENAME%"
 pdflatex -shell-escape -draftmode -synctex=1 -interaction=nonstopmode -interaction=nonstopmode -extra-mem-top=50000000  -extra-mem-bot=10000000  -main-memory=90000000  "%FILENAME%.tex"
 makeglossaries "%FILENAME%"
 pdflatex  -shell-escape -synctex=1 -interaction=nonstopmode -interaction=nonstopmode -extra-mem-top=50000000  -extra-mem-bot=10000000  -main-memory=90000000 "%FILENAME%.tex"
-
+MOVE /Y %FILENAME%.pdf %OUTPUT%.pdf
 countErrWrnBBx.bat "%FILENAME%.log"
 
