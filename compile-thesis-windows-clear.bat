afbeacb5dfec1aecb373336775827bc48a22e3ab
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2017-09-18 16:02:38 +0200

    some script improvements

--- compile-thesis-windows-clear.bat
+++ compile-thesis-windows-clear.bat
@@ -1,58 +1,55 @@
+SETLOCAL EnableExtensions EnableDelayedExpansion
 SET FILENAME=thesis
 rem MIKTEX_BIN=C:\miktex\texmfs\install\miktex\bin\
 rem set PATH=%MIKTEX_BIN%;%PATH%
+set ROOT_DIR=%~dp0
+cd %ROOT_DIR%
+DEL /Q "%FILENAME%.aux"  >nul 2>&1
+DEL /Q "%FILENAME%.bbl"  >nul 2>&1
+DEL /Q "%FILENAME%.blg"  >nul 2>&1
+DEL /Q "%FILENAME%.d" >nul 2>&1
+DEL /Q "%FILENAME%.fls" >nul 2>&1
+DEL /Q "%FILENAME%.ild" >nul 2>&1
+DEL /Q "%FILENAME%.ind" >nul 2>&1
+DEL /Q "%FILENAME%.toc" >nul 2>&1
+DEL /Q "%FILENAME%.lot" >nul 2>&1
+DEL /Q "%FILENAME%.lof" >nul 2>&1
+DEL /Q "%FILENAME%.idx" >nul 2>&1
+DEL /Q "%FILENAME%.out" >nul 2>&1
+DEL /Q "%FILENAME%.nlo" >nul 2>&1
+DEL /Q "%FILENAME%.nls" >nul 2>&1
+DEL /Q "%FILENAME%.pdf" >nul 2>&1
+DEL /Q "%FILENAME%.ps" >nul 2>&1
+DEL /Q "%FILENAME%.dvi" >nul 2>&1
+DEL /Q "%FILENAME%.bcf" >nul 2>&1
+DEL /Q "%FILENAME%.aux.bbl" >nul 2>&1
+DEL /Q "%FILENAME%.aux.blg" >nul 2>&1
 
-DEL "%FILENAME%.aux"
-DEL "%FILENAME%.bbl"
-DEL "%FILENAME%.blg"
-DEL "%FILENAME%.d"
-DEL "%FILENAME%.fls"
-DEL "%FILENAME%.ild"
-DEL "%FILENAME%.ind"
-DEL "%FILENAME%.toc"
-DEL "%FILENAME%.lot"
-DEL "%FILENAME%.lof"
-DEL "%FILENAME%.idx"
-DEL "%FILENAME%.out"
-DEL "%FILENAME%.nlo"
-DEL "%FILENAME%.nls"
-DEL "%FILENAME%.pdf"
-DEL "%FILENAME%.ps"
-DEL "%FILENAME%.dvi"
-DEL "%FILENAME%.bcf"
-DEL "%FILENAME%.aux.bbl"
-DEL "%FILENAME%.aux.blg"
+DEL /Q "%FILENAME%.aux" >nul 2>&1
+DEL /Q "%FILENAME%.acn" >nul 2>&1
+DEL /Q "%FILENAME%.acr" >nul 2>&1
+DEL /Q "%FILENAME%.alg" >nul 2>&1
+DEL /Q "%FILENAME%.auxlock" >nul 2>&1
+DEL /Q "%FILENAME%.bbl" >nul 2>&1
+DEL /Q "%FILENAME%.blg" >nul 2>&1
+DEL /Q "%FILENAME%.d" >nul 2>&1
+DEL /Q "%FILENAME%.fls" >nul 2>&1
+DEL /Q "%FILENAME%.ild" >nul 2>&1
+DEL /Q "%FILENAME%.ilg" >nul 2>&1
+DEL /Q "%FILENAME%.ind" >nul 2>&1
+DEL /Q "%FILENAME%.ist" >nul 2>&1
+DEL /Q "%FILENAME%.toc" >nul 2>&1
+DEL /Q "%FILENAME%.lot" >nul 2>&1
+DEL /Q "%FILENAME%.lof" >nul 2>&1
+DEL /Q "%FILENAME%.idx" >nul 2>&1
+DEL /Q "%FILENAME%.out" >nul 2>&1
+DEL /Q "%FILENAME%.pre" >nul 2>&1
+DEL /Q "%FILENAME%.nlo" >nul 2>&1
+DEL /Q "%FILENAME%.nls" >nul 2>&1
+DEL /Q "%FILENAME%.slg" >nul 2>&1
+DEL /Q "%FILENAME%.syg" >nul 2>&1
+DEL /Q "%FILENAME%.syi" >nul 2>&1
+DEL /Q "%FILENAME%.bcf" >nul 2>&1
+DEL /Q "%FILENAME%.aux.bbl" >nul 2>&1
+DEL /Q "%FILENAME%.aux.blg" >nul 2>&1
 
-
-
-DEL "%FILENAME%.aux"
-DEL "%FILENAME%.acn"
-DEL "%FILENAME%.acr"
-DEL "%FILENAME%.alg"
-DEL "%FILENAME%.auxlock"
-DEL "%FILENAME%.bbl"
-DEL "%FILENAME%.blg"
-DEL "%FILENAME%.d"
-DEL "%FILENAME%.fls"
-DEL "%FILENAME%.ild"
-DEL "%FILENAME%.ilg"
-DEL "%FILENAME%.ind"
-DEL "%FILENAME%.ist"
-DEL "%FILENAME%.toc"
-DEL "%FILENAME%.lot"
-DEL "%FILENAME%.lof"
-DEL "%FILENAME%.idx"
-DEL "%FILENAME%.out"
-DEL "%FILENAME%.pre"
-DEL "%FILENAME%.nlo"
-DEL "%FILENAME%.nls"
-DEL "%FILENAME%.slg"
-DEL "%FILENAME%.syg"
-DEL "%FILENAME%.syi"
-DEL "%FILENAME%.bcf"
-DEL "%FILENAME%.aux.bbl"
-DEL "%FILENAME%.aux.blg"
-
-
-
-#"%FILENAME%.pdf"
